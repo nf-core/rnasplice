d4a87c391d02cf50b8893bca1349904a89a8b910
claireprince <Claire.Prince@zifornd.com>
2022-11-29 16:01:57 +0000

    SL-15 Implement featurecounts for DEXSeq DEU

--- bin/run_dexseq_exon.R
+++ bin/run_dexseq_exon.R
@@ -2,6 +2,8 @@
 
 library(DEXSeq)
 library(BiocParallel)
+library(dplyr)
+library(purrr)
 
 args <- commandArgs(trailingOnly=TRUE)
 
@@ -23,7 +25,7 @@ samplesheet    <- args[3] # samplesheet
 read_method    <- args[4] # either HTSeq or featurecounts
 ncores         <- args[5] # MultiCoreParam ncores
 
-if (length(args) == 6) {
+if (length(args) == 7) {
 
     denominator <- args[6]  # denominator for lfc set by user
 
@@ -59,6 +61,79 @@ samps <- samps[,c("sample", "condition")]
 # filter for unique rows based on sample name
 samps <- samps[!duplicated(samps[,"sample"]),]
 
+#################################################
+######## Define featurescounts function #########
+#################################################
+
+## Load Fcount output from : DEXSeq_after_Fcount.R into DEXSeq
+## Copyright 2015 Vivek Bhardwaj (bhardwaj@ie-freiburg.mpg.de). Licence: GPLv3.
+
+suppressPackageStartupMessages({
+    require(DEXSeq)
+})
+
+## Read Fcount output and convert to dxd
+DEXSeqDataSetFromFeatureCounts <- function (countfile, sampleData,
+                                            design = ~sample + exon + condition:exon, flattenedfile = NULL)
+
+    {
+        # Take a fcount file and convert it to dcounts for dexseq
+        message("Reading and adding Exon IDs for DEXSeq")
+        for (file in countFiles){
+            if (!exists("dcounts")){
+                dcounts <- read.table(file, skip=2)
+            } else {
+                temp_dcounts <-read.table(file, skip=2)
+                dcounts<-merge(dcounts, temp_dcounts, by.x = "V1", by.y = "V1")
+                rm(temp_dcounts)
+            }
+        }
+        colnames(dcounts) <- c("GeneID", rownames(sampleData) )
+        rownames(dcounts) <- as.character(dcounts[,1])
+        dcounts <- dcounts[,2:ncol(dcounts)]
+        dcounts <- dcounts[substr(rownames(dcounts), 1, 1) != "_", ] #remove _ from beginnning of gene name
+
+        ## get genes and exon names out
+        splitted <- strsplit(rownames(dcounts), ":")
+        exons <- sapply(splitted, "[[", 2)
+        genesrle <- sapply(splitted, "[[", 1)
+
+        ## parse the flattened file
+        if (!is.null(flattenedfile)) {
+            aggregates <- read.delim(flattenedfile, stringsAsFactors = FALSE, header = FALSE)
+            colnames(aggregates) <- c("chr", "source", "class", "start", "end", "ex", "strand", "ex2", "attr")
+            aggregates$strand <- gsub("\\.", "*", aggregates$strand)
+            aggregates <- aggregates[which(aggregates$class == "exonic_part"), ]
+            aggregates$attr <- gsub("\"|=|;", "", aggregates$attr)
+            aggregates$gene_id <- sub(".*gene_id\\s(\\S+).*", "\\1", aggregates$attr)
+            # trim the gene_ids to 255 chars in order to match with featurecounts
+            longIDs <- sum(nchar(unique(aggregates$gene_id)) > 255)
+            warning(paste0(longIDs, " aggregate geneIDs were found truncated in featureCounts output"), call. = FALSE)
+            aggregates$gene_id <- substr(aggregates$gene_id,1,255)
+
+            transcripts <- gsub(".*transcripts\\s(\\S+).*", "\\1", aggregates$attr)
+            transcripts <- strsplit(transcripts, "\\+")
+            exonids <- gsub(".*exonic_part_number\\s(\\S+).*", "\\1", aggregates$attr)
+            exoninfo <- GRanges(as.character(aggregates$chr), IRanges(start = aggregates$start, end = aggregates$end), strand = aggregates$strand)
+            names(exoninfo) <- paste(aggregates$gene_id, exonids, sep = ":")
+
+            names(transcripts) <- names(exoninfo)
+            if (!all(rownames(dcounts) %in% names(exoninfo))) {
+                    stop("Count files do not correspond to the flattened annotation file")
+            }
+            matching <- match(rownames(dcounts), names(exoninfo))
+            stopifnot(all(names(exoninfo[matching]) == rownames(dcounts)))
+            stopifnot(all(names(transcripts[matching]) == rownames(dcounts)))
+            dxd <- DEXSeqDataSet(dcounts, sampleData, design, exons, genesrle, exoninfo[matching], transcripts[matching])
+            return(dxd)
+        }
+        else {
+            dxd <- DEXSeqDataSet(dcounts, sampleData, design, exons, genesrle)
+            return(dxd)
+        }
+
+}
+
 ######################################
 ######## Run DEXseq analysis #########
 ######################################
@@ -81,13 +156,10 @@ if (read_method == "htseq"){
 
 if (read_method == "featurecounts"){
 
-     source("load_SubreadOutput.R")
-
-     dxd <- DEXSeqDataSetFromFeatureCounts(countfiles = countFiles,
-                                           sampleData = samps,
-                                           design = fullModel,
-                                           flattenedfile = flattenedFile)
-
+    dxd <- DEXSeqDataSetFromFeatureCounts(countfile = countFiles,
+                                        sampleData = samps,
+                                        design = fullModel,   
+                                        flattenedfile = flattenedFile)
  }
 
 dxd <- DEXSeq::estimateSizeFactors(dxd)
