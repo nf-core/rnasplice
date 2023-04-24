50e74ecc96921c4f6c06fb3b128561dfd64f8d5b
valentinoruggieri <valentino.ruggieri@zifornd.com>
2023-04-24 10:20:35 +0200

    fixed extension in check_input_type

--- bin/check_input_type.py
+++ bin/check_input_type.py
@@ -2,6 +2,8 @@
 
 import argparse
 import pandas as pd
+import os
+
 
 # Define command-line arguments
 parser = argparse.ArgumentParser(description='Validate a samplesheet file and output files with different column sets')
@@ -16,6 +18,7 @@ salmon_columns = ['sample', 'salmon', 'condition']
 
 # Load the samplesheet file
 samplesheet = pd.read_csv(args.samplesheet_file)
+base_samplesheet = os.path.splitext(args.samplesheet_file)[0]
 
 # Check that required columns exist and are not empty
 required_columns = ['sample', 'condition']
@@ -30,17 +33,17 @@ for col in required_columns:
 # Check which column set to output based on the available columns
 if all(col in samplesheet.columns and not samplesheet[col].isna().all() for col in ['fastq_1', 'fastq_2']):
     # Output the fastq column set
-    samplesheet[fastq_columns].to_csv(args.samplesheet_file + '.fastq.csv', index=False)
+    samplesheet[fastq_columns].to_csv(base_samplesheet + '.fastq.csv', index=False)
 elif 'bam' in samplesheet.columns and not samplesheet['bam'].isna().all():
     if 'transcriptome' in samplesheet.columns and not samplesheet['transcriptome'].isna().all():
         # Output the transcriptome column set
-        samplesheet[transcriptome_columns].to_csv(args.samplesheet_file + '.transcriptome.csv', index=False)
+        samplesheet[transcriptome_columns].to_csv(base_samplesheet + '.transcriptome.csv', index=False)
     else:
         # Output the bam column set
-        samplesheet[bam_columns].to_csv(args.samplesheet_file + '.bam.csv', index=False)
+        samplesheet[bam_columns].to_csv(base_samplesheet + '.bam.csv', index=False)
 elif 'salmon' in samplesheet.columns and not samplesheet['salmon'].isna().all():
     # Output the salmon column set
-    samplesheet[salmon_columns].to_csv(args.samplesheet_file + '.salmon.csv', index=False)
+    samplesheet[salmon_columns].to_csv(base_samplesheet + '.salmon.csv', index=False)
 else:
     print('Error: no valid column set found in the samplesheet file')
     exit(1)
